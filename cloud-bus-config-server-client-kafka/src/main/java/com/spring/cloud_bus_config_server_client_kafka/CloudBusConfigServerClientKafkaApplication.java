b6051d1e248961506a6d16c6bd419948a2461e00
jivan.patil <jivan.patil@bestseller.com>
2025-06-30 22:39:06 +0200

    Add test repo for code change

---
+++ cloud-bus-config-server-client-kafka/src/main/java/com/spring/cloud_bus_config_server_client_kafka/CloudBusConfigServerClientKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloud_bus_config_server_client_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudBusConfigServerClientKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudBusConfigServerClientKafkaApplication.class, args);
+	}
+
+}
